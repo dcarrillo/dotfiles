eb5a55e82d694626f22ef6a3c66f396e9ba36593
dcarrillo <daniel.carrillo@gmail.com>
2020-04-05 11:43:56 +0200

    [polybar] Add a MVP task_manager module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-killall -q polybar
-while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
+pkill polybar
+pkill -f "task_manager --daemon"
+while pgrep -u "$(id -u)" -x polybar >/dev/null; do
+    sleep 0.5;
+done
 
 export TERMINAL_CMD="tilix --profile orange --new-process -e"
-WM_CONTROL="$(dirname "$0")/scripts/switch_window_state"
-export WM_CONTROL
+export WM_CONTROL="$(dirname "$0")/scripts/switch_window_state"
 
 for monitor in $(polybar --list-monitors | cut -d":" -f1); do
     export MONITOR=$monitor
     polybar top -c ~/.config/polybar/bar.ini >/dev/null  &
 done
 
+until pgrep -u "$(id -u)" -x polybar >/dev/null; do
+    sleep 0.5
+done
+
+~/.config/polybar/scripts/task_manager --daemon &
