61efb724a38e50a252bc33632de78cc103fa7a6f
dcarrillo <daniel.carrillo@gmail.com>
2020-04-10 16:17:05 +0200

    [polybar] Update dynamic configuration for task_manager

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,8 @@ done
 export TERMINAL_CMD="tilix --profile orange --new-process -e"
 export WM_CONTROL="$(dirname "$0")/scripts/switch_window_state"
 
+~/.config/polybar/scripts/task_manager --generate-config 20
+
 for monitor in $(polybar --list-monitors | cut -d":" -f1); do
     export MONITOR=$monitor
     polybar top -c ~/.config/polybar/bar.ini >/dev/null  &
@@ -18,4 +20,4 @@ until pgrep -u "$(id -u)" -x polybar >/dev/null; do
     sleep 0.5
 done
 
-~/.config/polybar/scripts/task_manager --daemon 15 &
+~/.config/polybar/scripts/task_manager --daemon &
