1712274793b6f18064ffa19f9d9337514d4ed297
Daniel Carrillo <daniel.carrillo@gmail.com>
2022-10-07 19:47:47 +0200

    [neovim] Set separator for lualine components

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -58,7 +58,7 @@ lualine.setup({
 		globalstatus = true,
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
+		component_separators = { left = "", right = " " },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard" },
 		always_divide_middle = true,
