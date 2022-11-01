5a13d12e33d80c42daed96f723f59df6e6c92d7b
Daniel Carrillo <daniel.carrillo@gmail.com>
2022-11-01 18:14:36 +0100

    [neovim] Disable illuminate on very large files

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -32,3 +32,13 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 		]])
 	end,
 })
+
+--  Disable illuminate on very large files
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+	callback = function()
+		local line_count = vim.api.nvim_buf_line_count(0)
+		if line_count >= 5000 then
+			vim.cmd("IlluminatePauseBuf")
+		end
+	end,
+})
