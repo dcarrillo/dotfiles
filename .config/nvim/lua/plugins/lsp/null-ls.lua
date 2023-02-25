5f49f56c07929d22d68c0cb83646a665f95abb8a
Daniel Carrillo <daniel.carrillo@gmail.com>
2023-02-25 20:36:46 +0100

    [neovim] Add revive golang linter

--- .config/nvim/lua/plugins/lsp/null-ls.lua
+++ .config/nvim/lua/plugins/lsp/null-ls.lua
@@ -8,15 +8,24 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local revive_conf = vim.fn.findfile(os.getenv("HOME") .. "/.revive.toml")
+local revive_args = { "-formatter", "json", "./..." }
+if revive_conf then
+	revive_args = { "-formatter", "json", "-config", revive_conf, "./..." }
+end
+
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.black.with({
+			extra_args = { "--fast" },
+		}),
 		formatting.stylua,
 		formatting.prettier,
-		-- diagnostics.golangci_lint.with({ extra_args = { "--fast" } }),
-		diagnostics.cfn_lint,
 		diagnostics.hadolint,
 		diagnostics.markdownlint,
+		diagnostics.revive.with({
+			args = revive_args,
+		}),
 	},
 })
