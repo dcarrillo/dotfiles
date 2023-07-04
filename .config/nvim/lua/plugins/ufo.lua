a88d10dbcaf3926b9875b1a801e6eb049f9fa2f2
Daniel Carrillo <daniel.carrillo@gmail.com>
2023-07-04 19:09:25 +0200

    [neovim] Remove noisy comments from options.lua

--- .config/nvim/lua/plugins/ufo.lua
+++ .config/nvim/lua/plugins/ufo.lua
@@ -1,4 +1,6 @@
-require("ufo").setup({
+local ufo = require("ufo")
+
+ufo.setup({
 	provider_selector = function(bufnr, filetype, buftype)
 		return { "treesitter", "indent" }
 	end,
@@ -6,8 +8,8 @@ require("ufo").setup({
 
 local keymap = vim.keymap.set
 -- Code Folding
--- keymap("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
--- keymap("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
-keymap("n", "zr", require("ufo").openFoldsExceptKinds, { desc = "Fold less" })
-keymap("n", "zm", require("ufo").closeFoldsWith, { desc = "Fold more" })
-keymap("n", "zp", require("ufo").peekFoldedLinesUnderCursor, { desc = "Peek fold" })
+keymap("n", "zR", require("ufo").openAllFolds, { desc = "Open all folds" })
+keymap("n", "zM", require("ufo").closeAllFolds, { desc = "Close all folds" })
+keymap("n", "zr", ufo.openFoldsExceptKinds, { desc = "Fold less" })
+keymap("n", "zm", ufo.closeFoldsWith, { desc = "Fold more" })
+keymap("n", "zp", ufo.peekFoldedLinesUnderCursor, { desc = "Peek fold" })
