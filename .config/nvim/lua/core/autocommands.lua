78d7f98156fa8be88f97b26c1723c1f548876f8f
Daniel Carrillo <daniel.carrillo@gmail.com>
2023-10-13 17:52:38 +0200

    [neovim] Disable heavy plugins on very large files

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -24,16 +24,6 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 	end,
 })
 
--- Disable illuminate on very large files
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-	callback = function()
-		local line_count = vim.api.nvim_buf_line_count(0)
-		if line_count >= 5000 then
-			vim.cmd("IlluminatePauseBuf")
-		end
-	end,
-})
-
 -- Ensure terraform files use hcl LSP
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 	pattern = { "*.tf" },
@@ -41,5 +31,28 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
 		vim.cmd([[
 			set filetype=hcl
 		]])
+	end,
+})
+
+-- Disable some plugins on very large files
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*" },
+	callback = function(args)
+		local highlighter = require("vim.treesitter.highlighter")
+		local ts_was_active = highlighter.active[args.buf]
+		local file_size = vim.fn.getfsize(args.file)
+		if file_size > 1024 * 1024 then
+			vim.cmd("TSBufDisable highlight")
+			vim.cmd("syntax off")
+			vim.cmd("syntax clear")
+			vim.cmd("IlluminatePauseBuf")
+			vim.cmd("NoMatchParen")
+			vim.cmd("UfoDisable")
+			vim.cmd("IBLDisable")
+			vim.cmd("LspStop")
+			if ts_was_active then
+				vim.notify("File larger than 1MB; syntax highlighting and heavy CPU use plugins turned off.")
+			end
+		end
 	end,
 })
