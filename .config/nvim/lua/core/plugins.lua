8d4f9cf2137be83a1e16b267deefa84908fc29e8
Daniel Carrillo <daniel.carrillo@gmail.com>
2023-12-12 20:21:45 +0100

    [neovim] Fix markdown preview

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -240,7 +240,9 @@ require("lazy").setup({
 	{ "mzlogin/vim-markdown-toc" },
 	{
 		"iamcco/markdown-preview.nvim",
-		build = "cd app && npm install",
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
