48a32697c9180a06235cc0097d609ea45912c89a
Daniel Carrillo <daniel.carrillo@gmail.com>
2024-03-22 20:24:21 +0100

    [neovim] Fix copilot Spelling custom commmand

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -16,7 +16,10 @@ require("copilot").setup({
 
 local prompts = {
 	Spelling = {
-		prompt = "/Spelling Please correct any grammar and spelling errors in the following text. Respect the markdown format when provided",
+		system_prompt =
+		"You are a technical writer. You have a deep understanding of both the English and the Spanish languages. You can effectively communicate complex ideas and concepts in a way everyone easily understands. You have a keen eye for detail and can pick up on subtle nuances in language that others may miss. You are also skilled at translating written or spoken content from one language to another, ensuring that the meaning and tone of the original message are preserved.",
+		prompt =
+		"Correct any grammar and spelling errors in the following text. Respect the markdown format when provided",
 	},
 }
 
