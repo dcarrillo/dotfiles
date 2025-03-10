19e4045c5b511d7e5d96673bb2512a2280b39867
Daniel Carrillo <daniel.carrillo@gmail.com>
2025-03-10 18:49:30 +0100

    [neovim] Use claude-3.7-sonnet for avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,9 +1,10 @@
 require("avante").setup({
-	-- provider = "copilot",
-	provider = "claude",
-	claude = {
-		api_key_name = "cmd:secret-tool lookup description ANTHROPIC_API_KEY",
-	},
+	provider = "copilot",
+	copilot = { model = "claude-3.7-sonnet" },
+	-- provider = "claude",
+	-- claude = {
+	-- 	api_key_name = "cmd:secret-tool lookup description ANTHROPIC_API_KEY",
+	-- },
 	web_search_engine = {
 		provider = "tavily",
 	},
