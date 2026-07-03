27d28c8e55db310650b45b9b94549f11a294c64e
Daniel Carrillo <daniel.carrillo@gmail.com>
2026-07-03 20:08:11 +0200

    [neovim] Update dependencies

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -246,6 +246,14 @@ require("lazy").setup({
 		},
 	},
 	-- {
+	-- 	"TabbyML/vim-tabby",
+	-- 	lazy = false,
+	-- 	init = function()
+	-- 		vim.g.tabby_agent_start_command = { "npx", "tabby-agent", "--stdio" }
+	-- 		vim.g.tabby_inline_completion_trigger = "manual"
+	-- 	end,
+	-- },
+	-- {
 	-- 	"qvalentin/helm-ls.nvim",
 	-- 	ft = "helm",
 	-- },
